aba91513c1650d6e156fdc53acc57ac8f4a64b73
Azizbek1202 <84729918+Azizbek1202@users.noreply.github.com>
2021-10-23 18:31:34 +0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     return (
         <div className="container-fluid">
             <div className="navbar">
-               <a href="#">Services</a>
-               <a href="#">Name Company</a>
-               <a href="#">Portfolio</a>
+               <a>Services</a>
+               <a>Name Company</a>
+               <a>Portfolio</a>
             </div>
             <div className="img">
                 <img src={img} alt="" />
